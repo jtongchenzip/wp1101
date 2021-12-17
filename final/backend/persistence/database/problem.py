acece4c68bc6cb33a93996bc1e9b3c52feaedfa4
daphne <>
2021-12-17 11:15:51 +0800

    add problem first

--- final/backend/persistence/database/problem.py
+++ final/backend/persistence/database/problem.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+import asyncpg
+
 from base import do
 import exceptions as exc
 
@@ -19,3 +25,28 @@ async def read(problem_id: int) -> do.Problem:
         raise exc.NotFound
     return do.Problem(id=id_, title=title, testcase_file_uuid=testcase_file_uuid,
                       description=description, start_time=start_time, end_time=end_time)
+
+
+"""
+fetchrow: 抓一筆 (read)
+fetch: 抓很多筆 (browse)
+execute: 不用抓資料 (edit, delete)
+"""
+
+
+async def add(title: str, start_time: datetime, end_time: datetime,
+              description: Optional[str], filename: str, testcase_file_uuid: UUID) -> int:
+    sql = (
+        fr"INSERT INTO problem"
+        fr"            (title, start_time, end_time, testcase_file_uuid, filename, description)"
+        fr"       VALUES (%(title)s, %(start_time)s, %(end_time)s, %(description)s, %(filename)s, %(s3_file_uuid)s)"
+    )
+
+    params = param_maker(title=title, start_time=start_time, end_time=end_time, description=description,
+                         filename=filename, testcase_file_uuid=testcase_file_uuid)
+    sql, params = pyformat2psql(sql, params)
+    try:
+        id_, = await pool_handler.pool.fetchrow(sql, *params)
+    except asyncpg.exceptions.UniqueViolationError:
+        raise exc.ProblemTitleExist
+    return id_
