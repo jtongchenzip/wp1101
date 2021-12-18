e66e26baf548f9af4b2ea6e6920a500a4cd916f7
daphne <>
2021-12-18 10:24:37 +0800

    set global css, modified Headers and ScoreTable

--- final/frontend/src/components/ui/Header.js
+++ final/frontend/src/components/ui/Header.js
@@ -1,18 +1,25 @@
-import * as React from 'react';
+import React from 'react';
+import { useSelector } from 'redux-thunk';
 import {
   AppBar, Toolbar, Typography, IconButton, Box,
 } from '@material-ui/core';
-// import LogoutIcon from '@material-ui/icons/ExitToAppRounded';
+import LogoutIcon from '@material-ui/icons/ExitToAppRounded';
 
-export default function ButtonAppBar({ title }) {
+export default function Header({ title }) {
+  // const user = useSelector((state) => state.user);
+  const student_id = 'B00000000';
+
   return (
     <Box>
-      <AppBar position="static" color="inherit" style={{ height: '64px' }}>
-        <Toolbar style={{ justifyContent: 'space-between', alignItems: 'center', margin: '0 12px' }}>
+      <AppBar position="static" color="inherit" style={{ minHeight: '64px' }}>
+        <Toolbar style={{ justifyContent: 'space-between', alignItems: 'center', margin: '5px 12px' }}>
           <Typography variant="h4">{title}</Typography>
-          {/* <IconButton size="medium" edge="end" aria-label="menu">
+          <IconButton size="medium" edge="end" aria-label="menu">
+            <div style={{ marginRight: 20 }}>
+              <Typography variant="h4">{student_id}</Typography>
+            </div>
             <LogoutIcon />
-          </IconButton> */}
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
