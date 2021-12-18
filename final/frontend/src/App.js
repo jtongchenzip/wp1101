e8dd13487aa233447a27ec601f18fc480f9fc33e
陳杰彤 <jtc@ntu.im>
2021-12-18 22:20:33 +0800

    merge main

--- final/frontend/src/App.js
+++ final/frontend/src/App.js
@@ -4,7 +4,6 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UIComponentUsage from './UIComponentUsage';
 import Login from './containers/auth/Login';
-import Register from './containers/auth/Register';
 import theme from './theme';
 
 export default function App() {
@@ -13,7 +12,7 @@ export default function App() {
       <CssBaseline />
       <Router>
         <Switch>
-          <Route exact path="/" component={Register} />
+          <Route exact path="/" component={Login} />
           <Route path="/ui-components" component={UIComponentUsage} />
         </Switch>
       </Router>
