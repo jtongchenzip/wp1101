ed600f749585b344b9e87ed94d573aec86034fcd
陳杰彤 <jtc@ntu.im>
2021-12-21 15:38:28 +0800

    haven't try read submission yet

--- final/backend/persistence/database/judge_case.py
+++ final/backend/persistence/database/judge_case.py
@@ -24,13 +24,12 @@ async def browse(submission_id: int) -> Sequence[do.JudgeCase]:
     sql = (
         fr"SELECT id, submission_id, title, description, state, error_message"
         fr"  FROM judge_case"
-        fr" WHERE submission_id= %(submission_id)s"
+        fr" WHERE submission_id = %(submission_id)s"
         fr" ORDER BY id ASC"
     )
     params = param_maker(submission_id=submission_id)
     sql, params = pyformat2psql(sql, params)
     records = await pool_handler.pool.fetch(sql, *params)
-    print(records)
     return [do.JudgeCase(id=id_, submission_id=submission_id, title=title, description=description,
                          state=state, error_message=error_message)
             for id_, submission_id, title, description, state, error_message in records]
