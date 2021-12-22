da53f7bad6587f1bef5129b3b139aadbf01d7bc2
daphne <>
2021-12-22 19:37:30 +0800

    login page api

--- final/frontend/src/containers/auth/Login.js
+++ final/frontend/src/containers/auth/Login.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import {
   Button, Typography, TextField, makeStyles,
 } from '@material-ui/core';
+import { logIn } from '../../actions/user/auth';
 
 const useStyles = makeStyles(() => ({
   main: {
@@ -21,21 +23,31 @@ const useStyles = makeStyles(() => ({
 
 export default function Login() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const user = useSelector((state) => state.user);
+
+  const handleLogIn = () => {
+    if (username.trim() !== '' && password.trim() !== '') {
+      dispatch(logIn(username.trim(), password.trim()));
+    }
+  };
 
   return (
     <>
       <div className={classes.main}>
-        <TextField id="outlined-required" label="Username" />
-        <TextField id="outlined-required" style={{ marginTop: 50 }} label="Password" />
+        <TextField id="outlined-required" label="Username" onChange={(e) => setUsername(e.target.value)} />
+        <TextField id="outlined-required" style={{ marginTop: 50 }} label="Password" onChange={(e) => setPassword(e.target.value)} />
         <div className={classes.buttonGroup}>
           <Button color="primary" variant="outlined">
             Register
           </Button>
-          <Link to="/ui-components">
-            <Button color="primary" variant="contained">
-              Login
-            </Button>
-          </Link>
+          <Button color="primary" variant="contained" onClick={handleLogIn}>
+            Login
+          </Button>
         </div>
         <Typography color="primary" style={{ marginTop: 20 }} variant="h8">
           Cypress Online Judge System
