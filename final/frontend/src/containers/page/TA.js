1d440717a64c449832c881cecb569b160e887f23
陳杰彤 <jtc@ntu.im>
2021-12-25 21:05:50 +0800

    TA done

--- final/frontend/src/containers/page/TA.js
+++ final/frontend/src/containers/page/TA.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Avatar, Button, Dialog, DialogActions, DialogContent, DialogTitle, makeStyles, TextField, Typography,
+  Avatar, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, makeStyles, TextField, Typography,
 } from '@material-ui/core';
 import CloudDownloadOutlined from '@material-ui/icons/CloudDownloadOutlined';
 import Settings from '@material-ui/icons/Settings';
@@ -35,7 +35,8 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     flexDirection: 'column',
     width: '15%',
-    paddingLeft: 30,
+    paddingLeft: 40,
+    paddingRight: 15,
   },
   hackAndIcon: {
     display: 'flex',
@@ -53,16 +54,56 @@ const useStyles = makeStyles(() => ({
 }));
 
 export default function TA() {
-  const [openProblemCard, setOpen] = useState(false);
+  const [openAddCard, setAddCardOpen] = useState(false);
+  const [openEditCard, setEditCardOpen] = useState(false);
   const [progress, setProgress] = useState(60);
   const [uploadFile, setUpLoadFile] = useState([]);
   const classes = useStyles();
 
   return (
     <>
+      <div className={classes.main}>
+        <div className={classes.leftSidebar}>
+          <Avatar alt="Pdogs" style={{ height: '60px', width: '60px' }} src="/static/images/avatar.jpg" />
+          <Typography color="primary" style={{ marginTop: 10 }} variant="h6">pdogs</Typography>
+          <Button type="text" variant="contained" disabled size="small" style={{ height: 25 }}>TA</Button>
+          {/* hack buttions need to change color to white */}
+          <Button color="primary" style={{ marginTop: 20 }} variant="text">Hack 1</Button>
+          <Button color="primary" style={{ marginTop: 10 }} variant="text">Hack 2</Button>
+          <Button color="primary" style={{ marginTop: 10 }} variant="text">Hack 3</Button>
+          <Button color="primary" style={{ marginTop: 10 }} variant="outlined" onClick={() => setAddCardOpen(true)}>Add</Button>
+        </div>
+        <div className={classes.scoreTableGroup}>
+          <ScoreTable />
+          <div className={classes.progressBarGroup}>
+            <Typography style={{ marginRight: 10 }} variant="body1">Task Completed</Typography>
+            <LinearProgressBar value={progress} />
+          </div>
+        </div>
+        <div className={classes.rightSidebar}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+            <div className={classes.hackAndIcon}>
+              <Typography style={{ marginRight: 10 }} variant="h4">Hackathon 1</Typography>
+              <IconButton onClick={() => setEditCardOpen(true)}>
+                <Settings htmlColor="grey" />
+              </IconButton>
+            </div>
+            <Typography style={{ marginTop: 15 }} variant="body1">Date : 2022/01/01</Typography>
+            <Typography style={{ marginTop: 5 }} variant="body1">Start Time : 09 : 10</Typography>
+            <Typography style={{ marginTop: 5 }} variant="body1">End Time : 12 : 10</Typography>
+            <div className={classes.stuAndIcon}>
+              <Typography style={{ marginRight: 10 }} variant="body1">Student Score</Typography>
+              <CloudDownloadOutlined htmlColor="grey" />
+            </div>
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Button color="primary" variant="contained">Submit</Button>
+          </div>
+        </div>
+      </div>
       <Dialog
-        open={openProblemCard}
-        onClose={() => setOpen(false)}
+        open={openAddCard}
+        onClose={() => setAddCardOpen(false)}
         maxWidth="md"
       >
         <DialogContent>
@@ -87,43 +128,33 @@ export default function TA() {
           </div>
         </DialogContent>
       </Dialog>
-      <div className={classes.main}>
-        <div className={classes.leftSidebar}>
-          <Avatar alt="Pdogs" style={{ height: '60px', width: '60px' }} src="/static/images/avatar.jpg" />
-          <Typography color="primary" style={{ marginTop: 10 }} variant="h6">pdogs</Typography>
-          <Button type="text" variant="contained" disabled size="small" style={{ height: 25 }}>TA</Button>
-          {/* hack buttions need to change color to white */}
-          <Button color="primary" style={{ marginTop: 20 }} variant="text">Hack 1</Button>
-          <Button color="primary" style={{ marginTop: 10 }} variant="text">Hack 2</Button>
-          <Button color="primary" style={{ marginTop: 10 }} variant="text">Hack 3</Button>
-          <Button color="primary" style={{ marginTop: 10 }} variant="outlined" onClick={() => setOpen(true)}>Add</Button>
-        </div>
-        <div className={classes.scoreTableGroup}>
-          <ScoreTable />
-          <div className={classes.progressBarGroup}>
-            <Typography style={{ marginRight: 10 }} variant="body1">Task Completed</Typography>
-            <LinearProgressBar value={progress} />
+      <Dialog
+        open={openEditCard}
+        onClose={() => setEditCardOpen(false)}
+        maxWidth="md"
+      >
+        <DialogContent>
+          <div className={classes.dialogContent} style={{ marginTop: 0 }}>
+            <Typography variant="body1">Title</Typography>
+            <TextField id="outlined-required" label="Title" />
           </div>
-        </div>
-        <div className={classes.rightSidebar}>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
-            <div className={classes.hackAndIcon}>
-              <Typography style={{ marginRight: 10 }} variant="h4">Hackathon 1</Typography>
-              <Settings htmlColor="grey" />
-            </div>
-            <Typography style={{ marginTop: 15 }} variant="body1">Date : 2022/01/01</Typography>
-            <Typography style={{ marginTop: 5 }} variant="body1">Start Time : 09 : 10</Typography>
-            <Typography style={{ marginTop: 5 }} variant="body1">End Time : 12 : 10</Typography>
-            <div className={classes.stuAndIcon}>
-              <Typography style={{ marginRight: 10 }} variant="body1">Student Score</Typography>
-              <CloudDownloadOutlined htmlColor="grey" />
-            </div>
+          <div className={classes.dialogContent} style={{ marginTop: 20 }}>
+            <Typography variant="body1">Start Time</Typography>
+            <DateTimePicker />
           </div>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Button color="primary" variant="contained">Submit</Button>
+          <div className={classes.dialogContent} style={{ marginTop: 20 }}>
+            <Typography variant="body1">End Time</Typography>
+            <DateTimePicker />
           </div>
-        </div>
-      </div>
+          <div className={classes.dialogContent} style={{ justifyContent: 'flex-start', marginTop: 10 }}>
+            <Typography style={{ marginRight: 90 }} variant="body1">Problem file</Typography>
+            <UploadButton setUpLoadFile={setUpLoadFile} />
+          </div>
+          <div className={classes.dialogContent} style={{ justifyContent: 'flex-end', marginTop: 0 }}>
+            <Button color="primary" variant="contained">Save</Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
