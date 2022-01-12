8693655a2713efac28b411d15aa6edd5ff65dc7c
daphne <>
2022-01-12 22:12:19 +0800

    import typo

--- final/frontend/src/reducers/loading/index.js
+++ final/frontend/src/reducers/loading/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import { auth } from './auth';
-import { problem } from './problem';
-import { submission } from './submission';
+import auth from './auth';
+import problem from './problem';
+import submission from './submission';
 
 export default combineReducers({
   auth,
