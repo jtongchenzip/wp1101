33c6d08a434a464496dfd5c1e82f26e8c417ec24
wsa-2002 <bensonwang2001@gmail.com>
2022-01-12 19:41:03 +0800

    remove useless code

--- final/frontend/src/UIComponentUsage.js
+++ final/frontend/src/UIComponentUsage.js
@@ -1,24 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react';
 import { Typography, Button, TextField } from '@material-ui/core';
-import moment from 'moment';
 import LinearProgressBar from './components/ui/LinearProgressBar';
 import ScoreTable from './components/ui/ScoreTable';
 import Header from './components/ui/Header';
 import UploadButton from './components/ui/UploadButton';
 import DateTimePicker from './components/ui/DateTimePicker';
-import {
-  readProblem, browseProblem, downloadStudentScore, readProblemLastSubmission,
-} from './actions/problem/problem';
 
 export default function App() {
   const [progress, setProgress] = useState(80);
   const [selectedDate, handleDateChange] = useState(new Date());
   const [uploadFile, setUpLoadFile] = useState([]);
 
-  const dispatch = useDispatch();
-  const problem = useSelector((state) => state.problem);
-  const submission = useSelector((state) => state.submission);
   // useEffect(() => {
   //   const timer = setInterval(() => {
   //     setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 10));
@@ -27,14 +19,7 @@ export default function App() {
   //     clearInterval(timer);
   //   };
   // }, []);
-  const test = () => {
-    dispatch(
-      readProblemLastSubmission(1, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoxLCJyb2xlIjoiVEEiLCJleHBpcmUiOiIyMDIyLTAxLTE5VDE1OjA2OjU2LjE0ODkzMSJ9.SaPufTe4-o9ISu6Czk_WbGfV2qVTMA259Gys6DQ7tmU'),
-    );
-  };
-  useEffect(() => {
-    console.log(submission);
-  }, [submission]);
+
   return (
     <>
       <Header title="Hackthon Online Judge System" />
@@ -51,7 +36,7 @@ export default function App() {
       <Button color="primary" variant="contained">
         Button
       </Button>
-      <Button color="primary" variant="contained" style={{ borderRadius: '15px' }} onClick={test}>
+      <Button color="primary" variant="contained" style={{ borderRadius: '15px' }}>
         15px
       </Button>
       <UploadButton setUpLoadFile={setUpLoadFile} />
