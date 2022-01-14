546a00d6544b5317250b156e8ad0c15965586487
wsa-2002 <bensonwang2001@gmail.com>
2022-01-14 23:43:23 +0800

    mock sql

--- final/database/zmock.sql
+++ final/database/zmock.sql
@@ -1,17 +1,7 @@
 INSERT INTO account (username, pass_hash, role, student_id, real_name)
-             VALUES ('wsa', '$argon2id$v=19$m=102400,t=2,p=8$8T6HMIaQklLK+d+bMyaEUA$gGDGdUJw8YC6SuxqFJxy4A', 'TA', 'b09705017', '王紹安');
+             VALUES ('TA', '$argon2id$v=19$m=65536,t=3,p=4$pDTG+B9jTOm9d26NMWYshQ$QFDRHpdxp0ns4QOQ8oJ3vGcOisjUA/Tne0wLm1S89pY', 'TA', 'b00000000', 'TA'); --password wp1101ta
 
 INSERT INTO account (username, pass_hash, role, student_id, real_name)
-             VALUES ('daphne', '$argon2id$v=19$m=102400,t=2,p=8$8T6HMIaQklLK+d+bMyaEUA$gGDGdUJw8YC6SuxqFJxy4A', 'TA', 'b09705005', '侯維書');
-
-INSERT INTO account (username, pass_hash, role, student_id, real_name)
-             VALUES ('jtc', '$argon2id$v=19$m=102400,t=2,p=8$8T6HMIaQklLK+d+bMyaEUA$gGDGdUJw8YC6SuxqFJxy4A', 'TA', 'b09705001', '陳杰彤');
-
-INSERT INTO s3_file (uuid, key, bucket)
-             VALUES ('7cbfc374-e0f8-42c4-b832-b0e75940f872', '7cbfc374-e0f8-42c4-b832-b0e75940f872', 'temp');
-
-INSERT INTO problem (title, testcase_file_uuid, filename, start_time, end_time, description)
-             VALUES ('hack1', '7cbfc374-e0f8-42c4-b832-b0e75940f872', 'cypress.zip', '2021-12-10', '2021-12-17', 'This is a test problem');
-
+             VALUES ('student', '$argon2id$v=19$m=65536,t=3,p=4$YYzR2tsbI+RcSwmhVIrxPg$ixa+C4bVEWFrRHH/eXpimzUBuasWwIYDLJWdDMg1CXw', 'STUDENT', 'b00000001', 'Student'); --password wp1101student
 
 
